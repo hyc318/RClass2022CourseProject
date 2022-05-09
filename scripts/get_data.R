3bc57efa53ccc6f3453cd52e6f05703093c93f65
hyc318 <hchamley@gmail.com>
2022-05-08 23:56:41 -0400

    update

--- scripts/get_data.R
+++ scripts/get_data.R
@@ -1,31 +1,23 @@
-# Download data from HUD
+# Libraries
 
+library(pacman)
 library(tidyverse)
 library(here)
+library(tidycensus)
 library(readxl)
-
+library(dplyr)
+library(janitor)
 # Output file names
 
-housing_data_joined_csv <- here("Data_Trans", "housing_data_joined.csv")
+births <- read_csv(here::here("Data", "births_usafacts.csv"))
+# https://usafacts.org/data/topics/people-society/health/longevity/births/
 
-# Check if we have files already downloaded
+net_worth <- read_csv(here::here("Data", "average_family_net_worth_usafacts.csv"))
+# https://usafacts.org/data/topics/economy/wealth-and-savings/wealth-profile/family-average-net-worth/
 
-if(file.exists(housing_data_joined_csv)) {
-  
-  return(cat("Housing files already exists"))
-  
-} else {
-  
-  
-  AK_data <- read_xlsx(here::here("Data", "TRACT_AK_MN_2020.xlsx"))
-  MO_data <- read_xlsx(here::here("Data", "TRACT_MO_WY_2020.xlsx"))
-  
-  joined_data <- AK_data %>% 
-    bind_rows(MO_data)
-  
-  # Save file
-  write_csv(joined_data, housing_data_joined_csv)
-}
+life_expect <- read_csv(here::here("Data", "life_expectancy_in_years_usafacts.csv"))
+# https://usafacts.org/data/topics/people-society/health/longevity/life-expectancy/
+
 
 
 
